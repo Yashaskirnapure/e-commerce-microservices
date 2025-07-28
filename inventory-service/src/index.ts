6b3be4680cb5b6c924949899af9f62d046b55824
Yashas Kirnapure <yashas.kirnapure@gmail.com>
2025-07-28 21:15:24 +0530

    init: basic scaffolding for inventory service

---
+++ inventory-service/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+dotenv.config();
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(express.json());
+app.use((err: any, req: Request, res: Response, next: Function) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Internal Server Error" });
+});
+
+app.listen(3002, () => { console.log("inventory service running on port 3002") })
