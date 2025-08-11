3bbe3aa571a4caf839628cf165f26ff9b08511c2
Yashas Kirnapure <yashas.kirnapure@gmail.com>
2025-08-11 10:09:05 +0530

    feat: added routes for inventory handling

--- inventory-service/src/routers/inventory.router.ts
+++ inventory-service/src/routers/inventory.router.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
-import { getStock } from '../controllers/inventory.controllers';
+import { getStock, sellStock, reserveStock, replenishStock, createStockEntry } from '../controllers/inventory.controllers';
 
 const inventoryRouter = Router();
-inventoryRouter.get('/get-stock/:productId/:state', getStock);
+inventoryRouter.get('/get-stock/:productId', getStock);
+inventoryRouter.patch('/reserve-stock/:productId', reserveStock);
+inventoryRouter.patch('/sell-stock/:productId', sellStock);
+inventoryRouter.patch('/replenish-stock/:productId', replenishStock);
+inventoryRouter.post('/create-stock-entry', createStockEntry);
 
 export default inventoryRouter;
